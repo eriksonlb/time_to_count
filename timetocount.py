4bb6879ec7addefb10f0bbe1e2291d2f79c5a960
erikson.bezerra <erikson.bezerra@dieboldnixdorf.com.br>
2019-10-11 17:00:54 -0300

    upgrades

--- timetocount.py
+++ timetocount.py
@@ -1,6 +1,7 @@
-from time import time
+import time
 from datetime import datetime 
 import os 
+# import counting
 
 clear = lambda: os.system('clear')
 exit = lambda: os.system('exit')
@@ -12,59 +13,62 @@ def verificador(x):
         mil = 'MIL =  (hh:mm:ss.ms) {}'.format(time_elapsed)
         return mil
 
-    if x == 10000:
+    elif x == 10000:
         time_elapsed = datetime.now() - start_time 
         dez_mil = 'DEZ MIL =  (hh:mm:ss.ms) {}'.format(time_elapsed)      
         return dez_mil
         
-    if x == 100000:
+    elif x == 100000:
         time_elapsed = datetime.now() - start_time 
         cem_mil = 'CEM MIL = (hh:mm:ss.ms) {}'.format(time_elapsed)
         return cem_mil
 
-    if x == 1000000:
+    elif x == 1000000:
         time_elapsed = datetime.now() - start_time 
         milhao = '1 MILHÃO = (hh:mm:ss.ms) {}'.format(time_elapsed)
         return milhao
 
-    if x == 10000000:
+    elif x == 10000000:
         time_elapsed = datetime.now() - start_time 
         dez_milhoes = 'DEZ MILHÕES = (hh:mm:ss.ms) {}'.format(time_elapsed)
         return dez_milhoes
 
-    if x == 100000000:
+    elif x == 100000000:
         time_elapsed = datetime.now() - start_time 
         cem_milhoes = 'CEM MILHÕES = (hh:mm:ss.ms) {}'.format(time_elapsed)
         return cem_milhoes
 
-    if x == 100000000:
+    elif x == 100000000:
         time_elapsed = datetime.now() - start_time 
         bilhao = '1 BILHÃO = (hh:mm:ss.ms) {}'.format(time_elapsed)
         return bilhao
-    if x == 100000000:
+    elif x == 100000000:
         time_elapsed = datetime.now() - start_time 
         dez_bilhoes = '10 BILHÕES (hh:mm:ss.ms) {}'.format(time_elapsed)
         return dez_bilhoes
 
-    if x == 1000000000:
+    elif x == 1000000000:
         time_elapsed = datetime.now() - start_time 
         cem_bilhoes = '100 BILHÕES = (hh:mm:ss.ms) {}'.format(time_elapsed)
         return cem_bilhoes
 
-    if x == 1000000000:
+    elif x == 1000000000:
         time_elapsed = datetime.now() - start_time 
         trilhao = '1 TRILHÃO = (hh:mm:ss.ms) {}'.format(time_elapsed)
         return trilhao
+    else:
+        return x
  
 decimais = []
 
 start_time = datetime.now() 
-for i in range(1, 1000000000001):
-    if len(decimais) > 0:
-        for n in decimais:
-            print(n)
-    print(i)
-    clear()
+for i in range(1, 100001):
+    decimais.append(verificador(i))
+    # clear()
+
+for a in decimais:
+    print(a)
+    time.sleep(0.05)
     
 
 
